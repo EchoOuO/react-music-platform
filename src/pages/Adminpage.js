eb6e620e1cfd0af184658fcc05c0fbe51368558c
isaacnetooo <isaacchapermannneto@gmail.com>
2024-04-09 00:33:51 -0700

    styled the admin page

--- src/pages/Adminpage.js
+++ src/pages/Adminpage.js
@@ -1,42 +1,43 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import FileService from "../services/FileService";
+import "../pages/Table.css";
 
 function AdminPage() {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios
-      .get("/data/users.json")
-      .then((response) => {
-        const data = response.data;
-        setUsers(
-          data.map(({ uid, uname, email }) => ({ id: uid, name: uname, email }))
-        );
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
+    //import user json data
+    FileService.read("user").then(
+      (response) => {
+        if (Array.isArray(response.data)) {
+          setUsers(
+            response.data.map((user) => ({
+              id: user.uid, // adjust the property name here
+              name: user.uname, // and here
+              email: user.email,
+              music: user.music,
+            }))
+          );
+        } else {
+          console.error("Error: response data is not an array");
+        }
+      },
+      (rej) => {
+        console.log(rej);
+      }
+    );
   }, []);
 
-  const handleAddUser = (newUser) => {
-    setUsers([...users, newUser]);
-  };
-
-  const handleRemoveUser = (userId) => {
-    setUsers(users.filter((user) => user.id !== userId));
-  };
   return (
     <div>
       <h1>Admin Page</h1>
-
-      <h2>Users</h2>
       <table className="table">
         <thead>
           <tr>
             <th>ID</th>
             <th>Name</th>
             <th>Email</th>
-            <th>Actions</th>
+            <th>Songs</th>
           </tr>
         </thead>
         <tbody>
@@ -46,20 +47,15 @@ function AdminPage() {
               <td>{user.name}</td>
               <td>{user.email}</td>
               <td>
-                <button
-                  className="btn btn-danger"
-                  onClick={() => handleRemoveUser(user.id)}
-                >
-                  Remove User
-                </button>
+                {Array.isArray(user.music) &&
+                  user.music.map((song, index) => (
+                    <p key={index}>{song.title}</p>
+                  ))}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <button className="btn btn-primary" onClick={handleAddUser}>
-        Add User
-      </button>
     </div>
   );
 }
